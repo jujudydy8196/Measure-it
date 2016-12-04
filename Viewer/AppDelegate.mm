eac92fcdcbd86a32ffeb23a316bac172de04ca29
jujudydy8196 <jujudydy8196@gmail.com>
2016-12-04 16:18:12 -0500

    distance works version1

--- Viewer/AppDelegate.mm
+++ Viewer/AppDelegate.mm
@@ -47,8 +47,8 @@ void preventApplicationFromStartingInTheBackgroundWhenTheStructureSensorIsPlugge
 
     NSError* error = nil;
     NSString * yi_ip = @"128.237.133.140";
-//    NSString * judy_ip = @"128.237.134.40";
-    [STWirelessLog broadcastLogsToWirelessConsoleAtAddress:yi_ip usingPort:4999 error:&error];
+    NSString * judy_ip = @"128.237.165.200";
+    [STWirelessLog broadcastLogsToWirelessConsoleAtAddress:judy_ip usingPort:4999 error:&error];
 
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];

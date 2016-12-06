347180a36719c2327e90c7270d5d9eef08927c73
Yi Hua <yhua1@andrew.cmu.edu>
2016-12-05 19:07:06 -0500

    room capture duplicate

---
+++ Viewer/ViewController+SLAM_m.m
@@ -0,0 +1,13 @@
+//
+//  ViewController+SLAM_m.m
+//  MeasureIt
+//
+//  Created by Yi Hua on 12/5/16.
+//  Copyright © 2016 Occipital. All rights reserved.
+//
+
+#import "ViewController+SLAM_m.h"
+
+@implementation ViewController (SLAM_m)
+
+@end
